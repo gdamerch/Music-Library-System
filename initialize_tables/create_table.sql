02c41078fc4ae57039f78af4193be63dc9c6504e
gdamerch <gdamerchi@gmail.com>
2025-03-22 16:45:49 -0700

    Updated create_table.sql
    Fixed playlist attribute P_LENGTH (was previously P_DURATION) and added comments indicating what each attribute is supposed to be.

--- initialize_tables/create_table.sql
+++ initialize_tables/create_table.sql
@@ -1,38 +1,38 @@
 CREATE TABLE SONG (
-    S_SONGID INTEGER NOT NULL,
-    S_ALBUMID INTEGER NOT NULL,
-    S_ARTISTID INTEGER NOT NULL,
-    S_TITLE VARCHAR(999),
-    S_GENRE CHAR(99),
-    S_DURATION TIME
+    S_SONGID INTEGER NOT NULL,          -- Song's ID
+    S_ALBUMID INTEGER NOT NULL,         -- ID of the album the song is in
+    S_ARTISTID INTEGER NOT NULL,        -- ID of the artist the song was made by
+    S_TITLE VARCHAR(999),               -- Title of the song
+    S_GENRE CHAR(99),                   -- Genre of the song
+    S_DURATION TIME                     -- Duration of the song
 );
 
 CREATE TABLE PLAYLIST ( 
-    P_PLAYLISTID INTEGER NOT NULL,
-    P_UID INTEGER NOT NULL,
-    P_TITLE VARCHAR(999),
-    P_PRIVATE BOOLEAN NOT NULL,
-    P_CREATIONDATE DATE,
-    P_DURATION TIME
+    P_PLAYLISTID INTEGER NOT NULL,      -- Playlist's ID
+    P_UID INTEGER NOT NULL,             -- ID of the user who made the playlist
+    P_TITLE VARCHAR(999),               -- Title of the playlist
+    P_PRIVATE BOOLEAN NOT NULL,         -- Whether the playlist is private or public
+    P_CREATIONDATE DATE,                -- Date playlist was made
+    P_LENGTH INTEGER NOT NULL           -- Number of songs
 );
 
 CREATE TABLE USERS (
-    U_UID INTEGER NOT NULL,
-    U_USERNAME VARCHAR(50) NOT NULL,
-    U_EMAIL CHAR(999) NOT NULL,
-    U_PASSWORD CHAR(50) NOT NULL
+    U_UID INTEGER NOT NULL,             -- ID of the user
+    U_USERNAME VARCHAR(50) NOT NULL,    -- User's username
+    U_EMAIL CHAR(999) NOT NULL,         -- User's email
+    U_PASSWORD CHAR(50) NOT NULL        -- User's password
 );
 
 CREATE TABLE ALBUM (
-    A_ALBUMID INTEGER NOT NULL,
-    A_ARTISTID INTEGER NOT NULL,
-    A_NUMBEROFSONGS INTEGER NOT NULL,
-    A_RELEASEDATE DATE,
-    A_DURATION TIME
+    A_ALBUMID INTEGER NOT NULL,         -- ID of the album
+    A_ARTISTID INTEGER NOT NULL,        -- ID of the artist who made the album
+    A_NUMBEROFSONGS INTEGER NOT NULL,   -- Number of songs in album
+    A_RELEASEDATE DATE,                 -- Date album was released
+    A_DURATION TIME                     -- Duration of the albuim
 );
 
 CREATE TABLE ARTIST (
-    AR_ARTISTID INTEGER NOT NULL,
-    AR_NAME VARCHAR(99) NOT NULL,
-    AR_COUNTRY VARCHAR(99) NOT NULL
+    AR_ARTISTID INTEGER NOT NULL,       -- ID of the artist
+    AR_NAME VARCHAR(99) NOT NULL,       -- Name of the artist
+    AR_COUNTRY VARCHAR(99) NOT NULL     -- Artist's country of origin
 );
